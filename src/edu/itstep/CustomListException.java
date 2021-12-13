8215980bdb6e2565f3a09c50ee28eef3d13761fb
Oleksii Kot <akkot08@gmail.com>
2021-12-13 11:29:11 +0200

    add: constructors

--- src/edu/itstep/CustomListException.java
+++ src/edu/itstep/CustomListException.java
@@ -4,4 +4,19 @@ public class CustomListException extends Exception {
     public CustomListException(String message) {
         super("Ошибка*  " + message);
     }
+
+    public CustomListException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CustomListException(Throwable cause) {
+        super(cause);
+    }
+
+    public CustomListException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public CustomListException() {
+    }
 }
